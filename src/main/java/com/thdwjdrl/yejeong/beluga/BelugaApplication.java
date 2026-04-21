61663bf3f95ff04344ca98fe3898dcbcceb59159
송정기 <thdwjdrl401@gmail.com>
2026-04-21 17:46:21 +0900

    init

---
+++ src/main/java/com/thdwjdrl/yejeong/beluga/BelugaApplication.java
@@ -0,0 +1,13 @@
+package com.thdwjdrl.yejeong.beluga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelugaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thdwjdrl/yejeong/beluga/BelugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thdwjdrl.yejeong.beluga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
